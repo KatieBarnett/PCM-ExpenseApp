05c5a102872b15ccbbeec348751c524e7426c1f6
Andrew Lee <leeandre@au1.ibm.com>
2013-12-06 13:43:47 +1100

    Removed commented out code that is not necessary.

--- ExpensesApp/apps/ExpensesApp/common/js/view/history.js
+++ ExpensesApp/apps/ExpensesApp/common/js/view/history.js
@@ -1,48 +1,13 @@
 /**
- * This is the JavaScript controller for process trip
- * @author Andrew Lee
+ * This is the JavaScript controller for the history page
  */
 
 var History = (function() {
 	return {
 		init : function() {
 			console.log("History :: init");
-			// Load sample data
-			/*DB.addTrip("Melbourne ANZ", "01-10-13", "03-10-13", function() {
-				});
-			DB.addTrip("ANZ Pitt St Meeting", "25-09-13", "25-09-13", function() {
-			});
-			DB.addTrip("NCEVER Adelaide Go Live Meeting", "27-09-13", "27-09-13", function() {
-			});
-			DB.addTrip("Canberra DoD RFD", "10-09-13", "10-09-13", function() {
-			});
-			DB.addTrip("Melbourne ANZ", "07-09-13", "13-09-13", function() {
-			});
-			DB.addTrip("Melbourne ANZ", "01-09-13", "13-09-13", function() {
-			});
-			DB.addExpense("Public Transportation", "1DHTA", "images\\receipt-placeholder.gif", 4, function() {
-				console.log("added expense");
-			});
-			DB.addExpense("Public Transportation", "1DHTY", "images\\receipt-placeholder1.gif", 4, function() {
-				console.log("added expense");
-			});
-			DB.addExpense("Hotel", null, "images\\no-receipt.gif", 4, function() {
-				console.log("added expense");
-			});		
 			
-			DB.addClientCode("1DHTA", "ANZ", "1DHTA", function() {
-				console.log("added expense");
-			});
-			DB.addClientCode("1DHTY", "CBA", "1DHTY", function() {
-				console.log("added expense");
-			});
-			DB.addClientCode("1DHTH", "NCVER", "1DHTH", function() {
-				console.log("added expense");
-			});
-			*/
-			
-			
-/* Review below for history screen */			
+			/* Review below for history screen */			
 			DB.getProcessedTrips(function(data){
 				
 				//Populate trip list
@@ -62,10 +27,6 @@ var History = (function() {
 					tripDates.appendChild(document.createTextNode(data[i]["endDate"]));
 					tripLI.appendChild(tripDates);
 					tripUL.appendChild(tripLI);
-					
-					
-					
-					
 				}
 				$('#historyList').listview('refresh');
 				
