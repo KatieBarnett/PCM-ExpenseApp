af0e015e50ce4565f88f1adc7dca12871cbc79d7
Andrew Lee <leeandre@au1.ibm.com>
2013-12-13 11:22:28 +1100

    Added functionality for selecting a photo in the album.

--- ExpensesApp/apps/ExpensesApp/common/js/controllers/cameraFunctions.js
+++ ExpensesApp/apps/ExpensesApp/common/js/controllers/cameraFunctions.js
@@ -7,7 +7,7 @@ var CameraFunctions = (function() {
 		
 		getPhotoFromLibrary : function(){
 			
-			 navigator.camera.getPicture(CameraFunctions.onPhotoURISuccess, CameraFunctions.onFail,{ quality: 45, 
+			 navigator.camera.getPicture(CameraFunctions.onPhotoSelectURISuccess, CameraFunctions.onFail,{ quality: 45, 
 		        destinationType: navigator.camera.DestinationType.FILE_URI,
 		        sourceType: navigator.camera.PictureSourceType.SAVEDPHOTOALBUM,
 		        correctOrientation: true,
@@ -43,7 +43,58 @@ var CameraFunctions = (function() {
 		 
 		onFail : function(){
 			console.log("Failed to get image uri");
+		},
+		
+		/**
+		 * Function specific to the album photo select as it will continuously overwrite resize.jpg.
+		 * This function will copy the resize.jpg and rename it to another file and pass the file URI
+		 * to expense type so it can be displayed in the app properly.
+		 * @param imageURI, the initial URI for the image selected.
+		 */
+		onPhotoSelectURISuccess : function(imageURI) {
+			// Get the unique timestamp name
+			var uniqueId = imageURI.substring(imageURI.lastIndexOf("?") + 1) + ".jpg";
+			var cachePath = "";
+			// Set the cachePath to Android specific file location if device is Android
+			if (Utils.isAndroid()) {
+				cachePath = "Android/data/com.ExpensesApp/cache";
+			}
+			
+			// When the resolution of the file system is successful
+			var onResolveSuccess = function(entry) {
+				
+				// Execute when the request for the file system is successful
+				var onRequestSuccess = function(fileSys) {
+					
+					// Execute when the directory request is successful
+					var onDirectorySuccess = function(directory) {
+						
+						// Copy the resize.jpg file to the same location but rename it to the unique id.jpg
+						entry.copyTo(directory, uniqueId, function(newEntry) {
+							console.log("success entry path: " + newEntry.fullPath);
+							var selectedImageURIPath = newEntry.fullPath;
+							CameraFunctions.onPhotoURISuccess(selectedImageURIPath);
+						}, function(error) {
+							console.log("Error code for copy: " + error.code);
+						});
+					};
+					
+					// Get the directory to store the new file in the same location as where all the other photos are located
+					fileSys.root.getDirectory(cachePath, {create: false}, onDirectorySuccess, function(error) {
+						console.log("get directory failed " + error.code);
+					});
+				};
+				
+				// Request the file system to make sure the file can be stored in a persistent area
+				window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onRequestSuccess, function(error) {
+					console.log("request file system failed " + error.code);
+				});
+			};
+			
+			// Resolve the local file system to make sure that we can access the file location
+			window.resolveLocalFileSystemURI(imageURI, onResolveSuccess, function(error) {
+				console.log("resolve local fs failed " + error.code);
+			});
 		}
-
 	};
 }());
