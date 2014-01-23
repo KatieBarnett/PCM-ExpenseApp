0666ae4eb6049a938760296562a5a12a2b2df848
Shasha Pendit <shpendit@au1.ibm.com>
2014-01-24 10:52:30 +1100

    email log inner list creation and css update

--- ExpensesApp/apps/ExpensesApp/common/js/view/emailLog.js
+++ ExpensesApp/apps/ExpensesApp/common/js/view/emailLog.js
@@ -16,16 +16,16 @@ var EmailLog = (function() {
 				for(var i=0; i<data.length; i++){
 					// Populate email history list as a inner list for each email & date pair
 					// TODO: uncomment inner list creation and fix css so it displays correctly 
-//					var emailLI = document.createElement("li");
-//					var emailLI_UL = document.createElement("ul");
+					//var emailLI = document.createElement("li");
+					//var emailLI_UL = document.createElement("ul");
 					var emailLI_UL_LI1 = document.createElement("li");
 					emailLI_UL_LI1.appendChild(document.createTextNode("Email: " + " " + data[i].email));
-					emailUL.appendChild(emailLI_UL_LI1); // Change to emailULLI.appendChild
+					emailULLI.appendChild(emailLI_UL_LI1.appendChild); // Change to emailULLI.appendChild
 					var emailLI_UL_LI2 = document.createElement("li");
 					emailLI_UL_LI2.appendChild(document.createTextNode("Date: " + " " + data[i].processDate));
-					emailUL.appendChild(emailLI_UL_LI2); // Change to emailULLI.appendChild
-//					emailLI.appendChild(emailLI_UL);
-//					emailUL.appendChild(emailLI);
+					emailULLI.appendChild(emailLI_UL_LI2); // Change to emailULLI.appendChild
+					emailLI.appendChild(emailLI_UL);
+					emailUL.appendChild(emailLI);
 				}
 				$('#EmailList').listview('refresh');
 				
